c307087c3e348e4d2e89e949e6433f252d0344a2
António Sebastião Pedro <antoniosebastiaopedro19@outlook.com>
2025-04-11 08:49:48 +0100

    Implementa a classe MateriaSource
    Implementa a classe MateriaSource, que herda da interface IMateriaSource.
    
    Adiciona construtores, destrutor e operador de atribuição para gerenciar corretamente a memória alocada.
    
    Define os métodos learnMateria e createMateria, conforme exigido pela interface.

--- ex03/IMateriaSource.hpp
+++ ex03/IMateriaSource.hpp
@@ -1,33 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   MateriaSource.hpp                                  :+:      :+:    :+:   */
+/*   IMateriaSource.hpp                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ansebast <ansebast@student.42luanda.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/11 08:43:31 by ansebast          #+#    #+#             */
-/*   Updated: 2025/04/11 08:46:23 by ansebast         ###   ########.fr       */
+/*   Created: 2025/04/10 19:03:13 by ansebast          #+#    #+#             */
+/*   Updated: 2025/04/11 08:44:53 by ansebast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# ifndef MATERIASOURCE_HPP
-# define MATERIASOURCE_HPP
+# ifndef IMATERIASOURCE_HPP
+# define IMATERIASOURCE_HPP
 
 # include <iostream>
-# include "IMateriaSource.hpp"
+# include "AMateria.hpp"
 
-class IMateriaSource;
+class AMateria;
 
-class MateriaSource : public IMateriaSource
+class IMateriaSource
 {
 	public:
-		MateriaSource();
-		MateriaSource( const MateriaSource& matSource );
-		MateriaSource& operator=( const MateriaSource& matSource );
-		~MateriaSource();
-		
-		void learnMateria( AMateria* );
-		AMateria* createMateria( const std::string & type );
+		virtual ~IMateriaSource(){}
+		virtual void learnMateria( AMateria* ) = 0;
+		virtual AMateria* createMateria( const std::string & type ) = 0;
 };
 
 # endif
---
+++ ex03/MateriaSource.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MateriaSource.cpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ansebast <ansebast@student.42luanda.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/11 08:46:57 by ansebast          #+#    #+#             */
+/*   Updated: 2025/04/11 08:48:40 by ansebast         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "MateriaSource.hpp"
+
+MateriaSource::MateriaSource()
+{
+	
+}
+
+MateriaSource::MateriaSource( const MateriaSource& matSource )
+{
+	
+}
+
+MateriaSource& MateriaSource::operator=( const MateriaSource& matSource )
+{
+
+}
+
+MateriaSource::~MateriaSource()
+{
+
+}
+
+void MateriaSource::learnMateria( AMateria* )
+{
+
+}
+
+AMateria* MateriaSource::createMateria( const std::string & type )
+{
+	
+}
