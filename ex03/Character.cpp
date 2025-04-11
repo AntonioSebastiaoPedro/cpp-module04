a7e9292bbfdd2fb8f31190ef86ff77df88295cae
António Sebastião Pedro <antoniosebastiaopedro19@outlook.com>
2025-04-11 08:38:44 +0100

    Adiciona um construtor padrão à classe Character e um construtor que aceita um nome, inicializando o atributo name. Atualiza o operador de atribuição para copiar o nome do personagem.

--- ex03/Character.cpp
+++ ex03/Character.cpp
@@ -6,7 +6,7 @@
 /*   By: ansebast <ansebast@student.42luanda.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 17:40:45 by ansebast          #+#    #+#             */
-/*   Updated: 2025/04/11 08:25:14 by ansebast         ###   ########.fr       */
+/*   Updated: 2025/04/11 08:37:33 by ansebast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,17 @@
 
 Character::Character()
 {
+	this->name = "Default";
 	std::cout << "Character default constructor called\n";
+	this->inventory[0] = NULL;
+	this->inventory[1] = NULL;
+	this->inventory[2] = NULL;
+	this->inventory[3] = NULL;
+}
+
+Character::Character( std::string name ) : name(name)
+{
+	std::cout << "Character name constructor called\n";
 	this->inventory[0] = NULL;
 	this->inventory[1] = NULL;
 	this->inventory[2] = NULL;
@@ -31,6 +41,7 @@ Character& Character::operator=( const Character& character )
 {
 	if ( this != &character )
 	{
+		this->name = character.name;
 		for ( int i = 0; i < 4; i++ )
 		{
 			this->inventory[i] = character.inventory[i];
--- ex03/Character.hpp
+++ ex03/Character.hpp
@@ -6,7 +6,7 @@
 /*   By: ansebast <ansebast@student.42luanda.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 17:40:45 by ansebast          #+#    #+#             */
-/*   Updated: 2025/04/11 08:24:58 by ansebast         ###   ########.fr       */
+/*   Updated: 2025/04/11 08:37:50 by ansebast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,12 @@
 class Character : public ICharacter
 {
 	private:
+		std::string name;
 		AMateria* inventory[4];
 	
 	public:
 		Character();
+		Character( std::string name );
 		Character( const Character& character );
 		Character& operator=( const Character& Character );
 		~Character();
